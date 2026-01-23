30e8712060796f6899b65364da0fcfc45676a7a5
patel riya <rbp2318@gmail.com>
2026-01-23 13:07:47 +0530

    corsConfig

--- src/main/java/in/backend/portfolio/config/CorsConfig.java
+++ src/main/java/in/backend/portfolio/config/CorsConfig.java
@@ -5,15 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    public WebMvcConfigurer CorsConfig() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://endearing-madeleine-6775c9.netlify.app/")
-                        .allowedMethods("*");
-            }
-        };
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://endearing-madeleine-6775c9.netlify.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
